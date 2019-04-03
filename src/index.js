ee1acbb07459619d30ecf6c2fa3e39411c01a665
Шоборов Роман <shoborov@mindbox.ru>
2019-04-03 10:26:48 +0300

    React app base

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDom from "react-dom"
+import { Application } from "./ApplicationPresenter"
+
+ReactDom.render(
+    <Application/>,
+    document.getElementById("root")
+);
